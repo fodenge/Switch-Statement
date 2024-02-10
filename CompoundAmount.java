0366f4424bfdc6fe14cfb4a70d6eb5520977ede8
Swati Pandey <shrutipandeyofficial@gmail.com>
2024-02-10 23:21:54 +0530

    Edit CompoundAmount.java file

--- CompoundAmount.java
+++ CompoundAmount.java
@@ -1,7 +1,7 @@
 public class CompoundAmount {
     double compoundAmount;
     public double getCompoundAmount(double principal, int time, double rate) {
-
+        compoundAmount = principal * Math.pow((1 + rate/100), time);
         return compoundAmount;
     }
 }
